878a2900058e370ba153a243026024538eb7b9bc
sejunkim6370 <167095170+sejunkim6370@users.noreply.github.com>
2024-07-23 23:23:27 -0700

    Add files via upload

--- predict.py
+++ predict.py
@@ -2,7 +2,6 @@ import numpy as np
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Conv1D, AveragePooling1D, Dropout, MaxPooling1D, Flatten, Dense 
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 X_test = np.load('X_test.npy')
 y_test = np.load('y_test.npy')
